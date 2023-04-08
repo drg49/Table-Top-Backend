721543dc37907934e46c3fa850b129fe3747f94c
Danny Gavin <homevpn14@gmail.com>
2023-04-08 18:10:58 -0400

    Updating passwords field and changing the server to run on localhost

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from models import db
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(105), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
