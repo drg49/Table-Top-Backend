7e34d0009f52f93e3ea0f9732c8fe83da9b76094
Danny Gavin <homevpn14@gmail.com>
2023-04-16 14:31:19 -0400

    Updating length for user emails.:

--- models/user.py
+++ models/user.py
@@ -2,9 +2,9 @@ from models import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(105), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    password = db.Column(db.String(105), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
