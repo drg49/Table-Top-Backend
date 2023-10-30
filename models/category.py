73f03a6a2591a0229000621af3b3f0e293da83ae
drg49 <dan.gavin49@gmail.com>
2023-10-29 23:01:06 -0400

    Add restaurant and category model

---
+++ models/category.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    restaurants = db.relationship('Restaurant', backref='category', lazy=True)
---
+++ models/restaurant.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    review_stars = db.Column(db.Numeric(3, 1))
+    hours_of_operation = db.Column(db.String(255), nullable=False)
+    website_link = db.Column(db.String(255))
+    attributes = db.Column(db.JSON)
+    created_at = db.Column(db.TIMESTAMP, nullable=False, server_default=db.func.now())
+
+    category = db.relationship('Category', back_populates='restaurants')
